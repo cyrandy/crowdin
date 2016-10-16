52f91045b01bf9795bdb118a63f7cafb5b07e221
cyrandy <cyrandy@gmail.com>
2016-10-16 14:11:20 +0800

    test

--- scripts/mergetx.sh
+++ scripts/mergetx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 for d in resources/*/ ; do
-	pofile=$d"LC_MESSAGES/messages.po"
+	pofile=$d"messages.po"
 	jsonfile=$d"translations.json"
 	lang=`echo "${d#*/}" | sed 's/\/$//'`
 	if [ ! -f $pofile ]
 	then
-		pybabel init -i translations/translations.pot -d resources/ -l ${lang}
+		pybabel init -i translations/translations.pot -d resources/ -l ${lang} -o resources/${lang}/messages.po
 	else
-		pybabel update -N -i translations/translations.pot -d resources/ -l ${lang}
+		pybabel update -N -i translations/translations.pot -d resources/ -l ${lang} -o resources/${lang}/messages.po
 	fi
 	./node_modules/.bin/po2json -f mf -p --fallback-to-msgid $pofile $jsonfile
 done
